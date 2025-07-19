2ec27bd59c87110c3413ecc76840245924434f23
Niraj Joshi <infocodewithniraj@gmail.com>
2025-07-19 22:39:52 +0545

    aaaaaa

--- src/modules/home/ui/components/search-filter/subcategorymenu.tsx
+++ src/modules/home/ui/components/search-filter/subcategorymenu.tsx
@@ -1,5 +1,4 @@
 "use client";
-import Link from "next/link";
 import { CategoriesGetManyOutput } from "@/modules/categories/types";
 import { Category } from "@/payload-types";
 import { useRouter } from "next/navigation";
